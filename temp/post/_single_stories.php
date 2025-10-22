7cd2663ad83a4607f11c861b4bc35b4b7453dafa
Rytxxx <k.ryuto.5@icloud.com>
2025-10-21 20:00:46 -0700

    feat: implement Note.com inspired story layout and styling

--- temp/post/_single_stories.php
+++ temp/post/_single_stories.php
@@ -1,116 +1,73 @@
-<?php if (is_object_in_term($post->ID, 'stories_cate',array('studyabroad'))): ?>
-　　　　<section class="studyabroad">
-			<div class="container">
-				<?php the_content(); ?>
-			</div>
-　　　　</section>
-<section class="profile reset">
-			<div class="container">
-				<div class="profile-col">
-					<div>
-						<div class="thumbnail">
-							<p>
-								<?php if( get_field('stories_pic') ):?>
-								<img src="<?php the_field('stories_pic');?>" alt="">
-								<?php else:?>
-								<img src="<?php print get_template_directory_uri(); ?>/assets/image/common/noimage.png" alt="">			
-								<?php endif; ?>
-							</p>
-						</div>
+<article class="note-article">
+	<header class="note-header">
+		<div class="container">
+			<div class="note-meta">
+				<?php $terms = get_the_terms($post->ID, 'stories_cate'); ?>
+				<?php if($terms): ?>
+					<div class="note-category">
+						<?php foreach($terms as $term): ?>
+							<span class="category-tag"><?php echo $term->name; ?></span>
+						<?php endforeach; ?>
 					</div>
-					<div>
-						<table>
-							<tr>
-								<th>名前</th>
-								<td><?php the_title(); ?></td>
-							</tr>
-							<?php if( get_field('stories_communitycolleg') ):?>
-							<?php endif; ?>	
-							<?php if( get_field('stories_university') ):?>
-							<tr>
-								<th>大学名</th>
-								<td><?php the_field('stories_university'); ?></td>
-							</tr>
-							<?php endif; ?>	
-							<?php if( get_field('stories_company') ):?>
-							<tr>
-								<th>所属</th>
-								<td><?php the_field('stories_company'); ?></td>
-							</tr>
-							<?php endif; ?>	
-							<?php if( get_field('stories_post') ):?>
-							<tr>
-								<th>役職</th>
-								<td><?php the_field('stories_post'); ?></td>
-							</tr>
-							<?php endif; ?>	
-						</table>
+				<?php endif; ?>
+			</div>
+			
+			<h1 class="note-title">
+				<?php 
+				$custom_title = get_post_meta($post->ID, 'stories_title', true);
+				echo $custom_title ? nl2br($custom_title) : get_the_title();
+				?>
+			</h1>
+			
+			<div class="note-author">
+				<div class="author-avatar">
+					<?php if( get_field('stories_pic') ):?>
+						<img src="<?php the_field('stories_pic');?>" alt="<?php the_title(); ?>">
+					<?php else:?>
+						<img src="<?php print get_template_directory_uri(); ?>/assets/image/common/noimage.png" alt="<?php the_title(); ?>">			
+					<?php endif; ?>
+				</div>
+				<div class="author-info">
+					<div class="author-name"><?php the_title(); ?></div>
+					<div class="author-details">
+						<?php if( get_field('stories_university') ):?>
+							<span><?php the_field('stories_university'); ?></span>
+						<?php endif; ?>
+						<?php if( get_field('stories_major') ):?>
+							<span><?php the_field('stories_major'); ?></span>
+						<?php endif; ?>
+						<?php if( get_field('stories_company') ):?>
+							<span><?php the_field('stories_company'); ?></span>
+						<?php endif; ?>
+						<?php if( get_field('stories_post') ):?>
+							<span><?php the_field('stories_post'); ?></span>
+						<?php endif; ?>
 					</div>
 				</div>
 			</div>
-		</section>
-<?php else : ?>
-<section class="profile reset">
-			<div class="container">
-				<div class="profile-col">
-					<div>
-						<div class="thumbnail">
-							<p>
-								<?php if( get_field('stories_pic') ):?>
-								<img src="<?php the_field('stories_pic');?>" alt="">
-								<?php else:?>
-								<img src="<?php print get_template_directory_uri(); ?>/assets/image/common/noimage.png" alt="">			
-								<?php endif; ?>
-							</p>
-						</div>
-						<div class="tag">
-							<?php $terms = get_the_terms($post->ID, 'stories_tag'); ?>
-							<?php if($terms): ?>
-							<?php foreach($terms as $term): ?>
-							<?php echo '<a href="'.get_term_link($term->slug,'stories_tag').'">'.$term->name.'</a>'; ?>
-							<?php endforeach; ?>
-							<?php endif; ?>
-						</div>
-					</div>
-					<div>
-						<div class="cate">
-							<?php $terms = get_the_terms($post->ID, 'stories_cate'); ?>
-							<?php if($terms): ?>
-							<?php foreach($terms as $term): ?>
-							<?php echo '<a href="'.get_term_link($term->slug,'stories_cate').'">'.$term->name.'</a>'; ?>
-							<?php endforeach; ?>
-							<?php endif; ?>
-						</div>
-						<h1 class="title">
-							<?php echo nl2br(get_post_meta($post->ID, 'stories_title', true)); ?>
-						</h1>
-						<table>
-							<tr>
-								<th>名前</th>
-								<td><?php the_title(); ?></td>
-							</tr>
-							<?php if( get_field('stories_university') ):?>
-							<tr>
-								<th>大学名</th>
-								<td><?php the_field('stories_university'); ?></td>
-							</tr>
-							<?php endif; ?>	
-							<?php if( get_field('stories_company') ):?>
-							<tr>
-								<th>メジャー</th>
-								<td><?php the_field('stories_major'); ?></td>
-							</tr>
-							<?php endif; ?>	
-							<?php if( get_field('stories_post') ):?>
-							<tr>
-								<th>役職</th>
-								<td><?php the_field('stories_post'); ?></td>
-							</tr>
-							<?php endif; ?>	
-						</table>
-					</div>
+			
+			<?php $tags = get_the_terms($post->ID, 'stories_tag'); ?>
+			<?php if($tags): ?>
+				<div class="note-tags">
+					<?php foreach($tags as $tag): ?>
+						<a href="<?php echo get_term_link($tag->slug,'stories_tag'); ?>" class="tag-link">#<?php echo $tag->name; ?></a>
+					<?php endforeach; ?>
 				</div>
-			</div>
-		</section>
-	<?php the_content(); ?>
-<?php endif; ?>	
+			<?php endif; ?>
+		</div>
+	</header>
+	
+	<div class="note-content">
+		<div class="container">
+			<?php if (is_object_in_term($post->ID, 'stories_cate',array('studyabroad'))): ?>
+				<div class="content-body">
+					<?php the_content(); ?>
+				</div>
+			<?php else : ?>
+				<div class="content-body">
+					<?php the_content(); ?>
+				</div>
+			<?php endif; ?>
+		</div>
+	</div>
+</article>	
