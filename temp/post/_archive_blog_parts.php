f66cec261fecd621c169caff0e6e11e8a814752e
Rytxxx <k.ryuto.5@icloud.com>
2025-10-23 11:40:01 -0700

    feat: implement note-like UI for blog page

--- temp/post/_archive_blog_parts.php
+++ temp/post/_archive_blog_parts.php
@@ -1,20 +1,43 @@
-<div class="item">
-	<p class="thumbnail">
-		<a href="<?php the_permalink(); ?>">
-					<?php if ( has_post_thumbnail() ) : // サムネイルを持っているときの処理 ?>
-					<img src="<?php $img_id = get_post_thumbnail_id(); $img_thumbnail = wp_get_attachment_image_src( $img_id , 'thum1000' ); echo ''.$img_thumbnail[0].'';?>">
-					<?php else: // サムネイルを持っていないときの処理 ?>
-					<img src="<?php print get_template_directory_uri(); ?>/assets/image/common/noimage.png" alt="">		
-					<?php endif; ?>	
-		</a>  
-	</p>
-	<div>
-		<div class="cate">
-		  <?php $categories = get_the_category(); foreach( $categories as $cat ): ?>
-			<a href="<?php echo get_category_link($cat->term_id); ?>"><?php echo $cat->name; ?></a>
-			<?php endforeach;?>
+<article class="note-blog-card">
+	<a href="<?php the_permalink(); ?>" class="blog-card-link">
+		<div class="blog-card-image">
+			<?php if ( has_post_thumbnail() ) : ?>
+				<img src="<?php $img_id = get_post_thumbnail_id(); $img_thumbnail = wp_get_attachment_image_src( $img_id , 'large' ); echo ''.$img_thumbnail[0].'';?>" alt="<?php the_title(); ?>">
+			<?php else: ?>
+				<img src="<?php print get_template_directory_uri(); ?>/assets/image/common/noimage.png" alt="<?php the_title(); ?>">
+			<?php endif; ?>
 		</div>
-		<h2 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-		<div class="tag"><?php the_tags(''); ?></div>
-	</div>
-</div>
+		<div class="blog-card-content">
+			<div class="blog-card-meta">
+				<?php $categories = get_the_category(); if($categories): ?>
+					<div class="blog-card-categories">
+						<?php foreach( $categories as $cat ): ?>
+							<span class="blog-category-tag"><?php echo $cat->name; ?></span>
+						<?php endforeach;?>
+					</div>
+				<?php endif; ?>
+				<time class="blog-card-date" datetime="<?php echo get_the_date('c'); ?>">
+					<?php echo get_the_date('Y.m.d'); ?>
+				</time>
+			</div>
+			<h2 class="blog-card-title"><?php the_title(); ?></h2>
+			<?php if(has_excerpt()): ?>
+				<p class="blog-card-excerpt"><?php echo wp_trim_words(get_the_excerpt(), 20); ?></p>
+			<?php endif; ?>
+			<?php $tags = get_the_tags(); if($tags): ?>
+				<div class="blog-card-tags">
+					<?php 
+					$tag_count = 0;
+					foreach($tags as $tag): 
+						if($tag_count >= 3) break;
+					?>
+						<span class="blog-tag">#<?php echo $tag->name; ?></span>
+					<?php 
+						$tag_count++;
+					endforeach; 
+					?>
+				</div>
+			<?php endif; ?>
+		</div>
+	</a>
+</article>
--- temp/post/_single_blog.php
+++ temp/post/_single_blog.php
@@ -1,21 +1,54 @@
+<article class="note-blog-article">
+	<header class="note-blog-header">
 		<div class="container">
-			<div class="reset">
-				<p class="cate">
-					<?php $categories = get_the_category(); foreach( $categories as $cat ): ?>
-					<a href="<?php echo get_category_link($cat->term_id); ?>"><?php echo $cat->name; ?></a>
-					<?php endforeach;?>
-				</p>			
-				<h1 class="title"><?php the_title(); ?></h1>
-				<div class="tag"><?php the_tags(''); ?></div>
-				<p class="thumbnail">
-					<?php if ( has_post_thumbnail() ) : // サムネイルを持っているときの処理 ?>
-					<img src="<?php $img_id = get_post_thumbnail_id(); $img_thumbnail = wp_get_attachment_image_src( $img_id , 'thum1000' ); echo ''.$img_thumbnail[0].'';?>">
-					<?php else: // サムネイルを持っていないときの処理 ?>
-					<img src="<?php print get_template_directory_uri(); ?>/assets/image/common/noimage.png" alt="">		
-					<?php endif; ?>	
-				</p>
+			<div class="note-blog-meta">
+				<?php $categories = get_the_category(); if($categories): ?>
+					<div class="note-blog-category">
+						<?php foreach( $categories as $cat ): ?>
+							<span class="category-tag"><?php echo $cat->name; ?></span>
+						<?php endforeach;?>
+					</div>
+				<?php endif; ?>
 			</div>
-			<div class="post-area">
+			
+			<h1 class="note-blog-title"><?php the_title(); ?></h1>
+			
+			<div class="note-blog-author">
+				<div class="author-avatar">
+					<img src="<?php print get_template_directory_uri(); ?>/assets/image/common/author-default.png" alt="管理者">
+				</div>
+				<div class="author-info">
+					<div class="author-name">Beyond Japan</div>
+					<div class="author-details">
+						<span><?php echo get_the_date('Y年n月j日'); ?></span>
+					</div>
+				</div>
+			</div>
+			
+			<?php $tags = get_the_tags(); if($tags): ?>
+				<div class="note-blog-tags">
+					<?php foreach($tags as $tag): ?>
+						<a href="<?php echo get_tag_link($tag->term_id); ?>" class="tag-link">#<?php echo $tag->name; ?></a>
+					<?php endforeach; ?>
+				</div>
+			<?php endif; ?>
+		</div>
+	</header>
+	
+	<!-- メイン画像 -->
+	<?php if ( has_post_thumbnail() ) : ?>
+	<div class="note-blog-featured-image">
+		<div class="container">
+			<img src="<?php $img_id = get_post_thumbnail_id(); $img_thumbnail = wp_get_attachment_image_src( $img_id , 'full' ); echo ''.$img_thumbnail[0].'';?>" alt="<?php the_title(); ?>">
+		</div>
+	</div>
+	<?php endif; ?>
+	
+	<div class="note-blog-content">
+		<div class="container">
+			<div class="content-body">
 				<?php the_content(); ?>
 			</div>
 		</div>
+	</div>
+</article>
