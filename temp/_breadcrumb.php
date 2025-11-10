326d8b38286655c9c737a1a8495c22a5266d067e
Rytxxx <k.ryuto.5@icloud.com>
2025-11-09 17:09:02 -0800

    feat: rename blog section from "お役立ちブログ" to "ブログ"

--- temp/_breadcrumb.php
+++ temp/_breadcrumb.php
@@ -6,7 +6,7 @@
 			<?php the_archive_title(); ?>
 			<?php endif; ?>
 			<?php if(is_single() ) : ?>
-			<a href="/blog/">お役立ちブログ</a>
+			<a href="/blog/">ブログ</a>
 			<!--<?php
 			$category = get_the_category();
 			if ( $category[ 0 ] ) {
