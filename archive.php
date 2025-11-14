4c223f0857a67b108c0ccedbbd1531716599243f
Rytxxx <k.ryuto.5@icloud.com>
2025-11-14 14:54:58 -0800

    refactor: improve code formatting and restructure archive template logic
    - Standardized PHP code spacing and indentation throughout archive.php
    - Reorganized conditional logic for blog archive display with clearer variable extraction
    - Inlined blog slide template parts to consolidate related markup

--- archive.php
+++ archive.php
@@ -1,35 +1,43 @@
 <?php
 get_header();
-$cat_info = get_category($cat);
+$cat_info         = get_category( $cat );
+$blog_scope       = get_query_var( 'blog_scope' );
+$is_blog_archive  = ( get_post_type() === 'post' ) && ( is_archive() || is_single() || is_page( array( 'blog' ) ) );
 ?>
 <div class="post">
 	<article class="archive reset">
 	<?php if ( get_post_type() === 'post' ) : ?>
-	<?php if( is_archive() && 'all' !== get_query_var( 'blog_scope' ) ) : ?>
+	<?php if ( is_archive() && 'all' !== $blog_scope ) : ?>
 	<?php get_template_part( 'temp/post/_archive_blog_slide' ); ?>
 	<?php endif; ?>
-	<?php elseif (is_post_type_archive('stories')): ?>
+	<?php elseif ( is_post_type_archive( 'stories' ) ) : ?>
 	<?php get_template_part( 'temp/post/_archive_stories_slide' ); ?>
 	<?php endif; ?>
-		 <div class="container">
-			 <?php if (get_post_type() === 'stories'): ?><?php get_template_part( 'temp/post/_archive_stories' ); ?>
-			 <?php elseif (get_post_type() === 'news'): ?>
-			 <?php get_template_part( 'temp/post/_archive_news' ); ?>
-			 <?php elseif(is_archive() || is_single() ||is_page( array( 'blog' ) )) : ?>
-			 	<?php if ( 'all' === get_query_var( 'blog_scope' ) ) : ?>
-			 		<?php get_template_part( 'temp/post/_archive_blog_all' ); ?>
-			 	<?php else : ?>
-			 		<?php get_template_part( 'temp/post/_archive_blog' ); ?>
-			 	<?php endif; ?>
-			 <?php endif; ?>
-			 <?php if (is_post_type_archive('stories')): ?>
+
+	<?php if ( $is_blog_archive && 'all' !== $blog_scope ) : ?>
+		<?php get_template_part( 'temp/post/_archive_blog' ); ?>
+	<?php else : ?>
+		<div class="container">
+			<?php if ( get_post_type() === 'stories' ) : ?>
+				<?php get_template_part( 'temp/post/_archive_stories' ); ?>
+			<?php elseif ( get_post_type() === 'news' ) : ?>
+				<?php get_template_part( 'temp/post/_archive_news' ); ?>
+			<?php elseif ( is_archive() || is_single() || is_page( array( 'blog' ) ) ) : ?>
+				<?php if ( 'all' === $blog_scope ) : ?>
+					<?php get_template_part( 'temp/post/_archive_blog_all' ); ?>
+				<?php else : ?>
+					<?php get_template_part( 'temp/post/_archive_blog' ); ?>
+				<?php endif; ?>
+			<?php endif; ?>
+			<?php if ( is_post_type_archive( 'stories' ) ) : ?>
 			<!--　アーカイブページナビ -->
 			<div class="com_wp_pagenavi">
-				<?php if(function_exists('wp_pagenavi')) { wp_pagenavi(); } ?>
+				<?php if ( function_exists( 'wp_pagenavi' ) ) { wp_pagenavi(); } ?>
 			</div>
 			<!--　/アーカイブページナビ --> 
 			<?php endif; ?>
-		 </div>
+		</div>
+	<?php endif; ?>
 	</article>	
 </div>
 <?php get_footer(); ?>
--- temp/post/_archive_blog_slide.php
+++ temp/post/_archive_blog_slide.php
@@ -1,20 +1,46 @@
 <div class="picup slid_blog">
 	
 			<?php
-			$paged = (int) get_query_var('paged');
-			$args = array(
-				'posts_per_page' => 5,
-				'paged' => $paged,
-				'orderby' => 'post_date',
-				'order' => 'DESC',
-				'post_type' => 'post',
-				'post_status' => 'publish',
-				'category_name' => 'pickup' //カテゴリースラッグ
-			);
-			$the_query = new WP_Query($args);
-			if ( $the_query->have_posts() ) :
-			while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-			<?php get_template_part( 'temp/post/_archive_blog_parts' ); ?>
-			<?php endwhile; endif; ?>
-			<?php wp_reset_postdata(); ?>
+	$paged = (int) get_query_var('paged');
+	$args = array(
+		'posts_per_page' => 5,
+		'paged' => $paged,
+		'orderby' => 'post_date',
+		'order' => 'DESC',
+		'post_type' => 'post',
+		'post_status' => 'publish',
+		'category_name' => 'pickup' //カテゴリースラッグ
+	);
+	$the_query = new WP_Query($args);
+	if ( $the_query->have_posts() ) :
+	while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+			<div class="item">
+				<p class="thumbnail">
+					<a href="<?php the_permalink(); ?>">
+						<?php if ( has_post_thumbnail() ) : ?>
+							<?php
+								$img_id       = get_post_thumbnail_id();
+								$img_thumbnail = wp_get_attachment_image_src( $img_id, 'thum1000' );
+								echo '<img src="' . esc_url( $img_thumbnail[0] ) . '" alt="">';
+							?>
+						<?php else : ?>
+							<img src="<?php print get_template_directory_uri(); ?>/assets/image/common/noimage.png" alt="">
+						<?php endif; ?>
+					</a>
+				</p>
+				<div>
+					<div class="cate">
+						<?php $categories = get_the_category(); ?>
+						<?php if ( $categories ) : ?>
+							<?php foreach ( $categories as $cat ) : ?>
+								<a href="<?php echo esc_url( get_category_link( $cat->term_id ) ); ?>"><?php echo esc_html( $cat->name ); ?></a>
+							<?php endforeach; ?>
+						<?php endif; ?>
+					</div>
+					<h2 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+					<div class="tag"><?php the_tags( '' ); ?></div>
+				</div>
+			</div>
+	<?php endwhile; endif; ?>
+	<?php wp_reset_postdata(); ?>
 </div>
